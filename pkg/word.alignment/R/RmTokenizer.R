3030b10dc37233b706895deab11eb9d80d313ffa
ne_da978 <ne_da978@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-24 09:03:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/word-alignment@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/word.alignment/R/RmTokenizer.R
@@ -0,0 +1,13 @@
+RmTokenizer <-
+function(text,split=FALSE)
+{
+    rr=as.character(read.delim('http://www.um.ac.ir/~sarmad/ol.txt',sep='\n',encod='UTF-8',header=FALSE)[[1]])
+    text=gsub(rr[1],' ',text)
+    text=gsub(rr[2],' ',text)
+    text=gsub(rr[3],' ',text)
+    text=gsub("[[:punct:]]", " ", text)
+    #while(!identical(text,gsub('  ',' ',text))) text=gsub('  ',' ',text)
+   
+    if (split) text=strsplit(text,' ') # It is not needed for word_alignIBM1
+    return(text)
+}
